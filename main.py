c337a202cf5c0c591d2f50b28d856dfb41d0fcce
nakatomotoi <33854773+nakatomotoi@users.noreply.github.com>
2018-05-22 13:42:16 +0900

    make sample_nakatomotoi.html,edit main.py

--- main.py
+++ main.py
@@ -40,6 +40,29 @@ def youtube_sample():
 
     return render_template('sample_be-kan.html', subsc=subsc, num_video=num_video, num_view=num_view)
 
+@app.route('/twitter_sample')
+def twitter_sample():
+    import json
+    from requests_oauthlib import OAuth1Session
+    twitter = OAuth1Session(API_KEY,API_SECRET,ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
+
+    url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
+
+    params ={
+                'count' : 100,
+                'screen_name':'hikakin'
+            }
+    req = twitter.get(url, params = params)
+    timeline = json.loads(req.text)
+
+    tweet_text = []
+    tweet_time = []
+    for tweet in timeline:
+        tweet_text.append(tweet["text"])
+        tweet_time.append(tweet["created_at"])
+
+    return render_template('sample_nakatomotoi.html', tweet_text=tweet_text, tweet_time=tweet_time)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")

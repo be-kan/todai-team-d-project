ddfec92e0369f44c546e7f883dd2e033f471c90b
神戸隆太 <kambe3141@gmail.com>
2018-05-24 18:11:37 +0900

    return dummy data to hikakin.html

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 from flask import Flask, render_template, request
-import urllib
-import json
-from requests_oauthlib import OAuth1Session
 
-
-app = Flask(__name__, instance_relative_config=True)
-app.config.from_pyfile('flask.cfg')
+app = Flask(__name__)
 
 
 @app.route('/')
@@ -15,40 +10,7 @@ def index():
 
 @app.route('/hikakin')
 def kikakin():
-    return render_template('hikakin.html')
-
-
-@app.route('/youtube_sample')
-def youtube_sample():
-    # channel_url = 'https://www.googleapis.com/youtube/v3/channels?'
-    # play_list_url = 'https://www.googleapis.com/youtube/v3/playlistItems?'
-    # channel_id = 'UCZf__ehlCEBPop-_sldpBUQ'
-    # youtube_key = app.config['YOUTUBE_KEY']
-    #
-    # channel_param = {
-    #     'part': 'contentDetails',
-    #     'id': channel_id,
-    #     'key': youtube_key
-    # }
-    #
-    # paramStr = urllib.parse.urlencode(channel_param)
-    # with urllib.request.urlopen(channel_url + paramStr) as res:
-    #     channel_data = json.loads(res.read().decode("utf-8"))
-    #
-    # play_list_id = channel_data['items'][0]['contentDetails']['relatedPlaylists']['uploads']
-    #
-    # play_list_param = {
-    #     'part': 'snippet',
-    #     'playlistId': play_list_id,
-    #     'maxResults': 50,
-    #     'key': youtube_key
-    # }
-    #
-    # paramStr = urllib.parse.urlencode(play_list_param)
-    # with urllib.request.urlopen(play_list_url + paramStr) as res:
-    #     play_list_data = json.loads(res.read().decode("utf-8"))
-
-    recent_ranking = {
+    recent_youtube_ranking = {
       '1': {
         'image_url': 'http://i.ytimg.com/vi/WJzSBLCaKc8/hqdefault.jpg',  # OK
         'view_count': 68158348,
@@ -123,7 +85,7 @@ def youtube_sample():
       }
     }
 
-    old_ranking = {
+    old_youtube_ranking = {
       '1': {
         'image_url': 'http://i.ytimg.com/vi/LE-JN7_rxtE/hqdefault.jpg',  # OK
         'view_count': 42782625,
@@ -156,7 +118,112 @@ def youtube_sample():
       }
     }
 
-    return render_template('sample_be-kan.html', recent_ranking=recent_ranking, old_ranking=old_ranking)
+    recent_tweet_ranking = {
+        '1': {
+          'text': 'ついに公開！→【YouTubeテーマソング／ヒカキン＆セイキン 】https://youtu.be/WJzSBLCaKc8  @YouTubeさんから',  # OK
+          'retweet_count': 1133,
+          'favolite_count': 2019,
+          'date': '2015/08/14'
+        },
+        '2': {
+          'text': 'UUUMから新しいアプリを出しました！マイクラPEやモンストがどこでも話しながらマルチプレイ出来る『おしゃべりマルチ！』是非使ってみて下さい♪ https://appsto.re/jp/sK0o8.i',  # OK
+          'retweet_count': 943,
+          'favolite_count': 1781,
+          'date': '2015/08/20'
+        },
+        '3': {
+          'text': 'YouTubeテーマソング100万再生突破！ ご視聴ありがとうございます（ ;  ; ） http://youtu.be/WJzSBLCaKc8 ',  # OK
+          'retweet_count': 544,
+          'favolite_count': 1842,
+          'date': '2015/08/15'
+        },
+        '4': {
+          'text': '駄目な人のアラームはこちら 寝る(-_-)zzz',  # OK
+          'retweet_count': 526,
+          'favolite_count': 2062,
+          'date': '2015/08/13'
+        },
+        '5': {
+          'text': '夜通しの撮影終了。 髪の毛がスプレーでパリパリ。',  # OK
+          'retweet_count': 443,
+          'favolite_count': 1994,
+          'date': '2015/08/16'
+        }
+    }
+
+    old_tweet_ranking = {
+        '1': {
+          'text': '実家でOFFしてるんでテレビ見てるんですが、凄く可愛い子が出てて気になって調べたら桃色クローバーZってグループのセンターの子だとわかった。これは桃色クローバーZ応援するしかねぇな。。。',  # OK
+          'retweet_count':  142,
+          'favolite_count':29,
+          'date': '2012/12/28'
+        },
+        '2': {
+          'text': 'HIKAKIN×PDS×長渕蓮！ これからディナーo(^_^)o',  # OK
+          'retweet_count': 129,
+          'favolite_count': 69,
+          'date': '2012/12/27'
+        },
+        '3': {
+          'text': '地元のスーパーで中学生が声かけてくれた＼(^o^)／僕もセイキンもマスクしてたのにわかるって凄いね。改めてYouTubeやネットの力に驚かされました☆',  # OK
+          'retweet_count': 75,
+          'favolite_count': 42,
+          'date': '2012/12/29'
+        },
+        '4': {
+          'text': '変態マッサージャーマスオがみんなにマッサージを施し中',  # OK
+          'retweet_count': 71,
+          'favolite_count': 27,
+          'date': '2012/12/30'
+        },
+        '5': {
+          'text': '松井秀喜さんの引退会見改めて見たけど、カッコよすぎ。。。あの柔らかい感じとか言動の一つ一つから偉大さを感じる。レベルが違いすぎる。。。',  # OK
+          'retweet_count': 64,
+          'favolite_count': 20,
+          'date': '2012/12/28'
+        }
+    }
+
+
+    return render_template('hikakin.html',
+                            recent_youtube_ranking=recent_youtube_ranking,
+                            old_youtube_ranking=old_youtube_ranking,
+                            recent_tweet_ranking=recent_tweet_ranking,
+                            old_tweet_ranking=old_tweet_ranking
+                          )
+
+
+@app.route('/youtube_sample')
+def youtube_sample():
+    # channel_url = 'https://www.googleapis.com/youtube/v3/channels?'
+    # play_list_url = 'https://www.googleapis.com/youtube/v3/playlistItems?'
+    # channel_id = 'UCZf__ehlCEBPop-_sldpBUQ'
+    # youtube_key = app.config['YOUTUBE_KEY']
+    #
+    # channel_param = {
+    #     'part': 'contentDetails',
+    #     'id': channel_id,
+    #     'key': youtube_key
+    # }
+    #
+    # paramStr = urllib.parse.urlencode(channel_param)
+    # with urllib.request.urlopen(channel_url + paramStr) as res:
+    #     channel_data = json.loads(res.read().decode("utf-8"))
+    #
+    # play_list_id = channel_data['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+    #
+    # play_list_param = {
+    #     'part': 'snippet',
+    #     'playlistId': play_list_id,
+    #     'maxResults': 50,
+    #     'key': youtube_key
+    # }
+    #
+    # paramStr = urllib.parse.urlencode(play_list_param)
+    # with urllib.request.urlopen(play_list_url + paramStr) as res:
+    #     play_list_data = json.loads(res.read().decode("utf-8"))
+
+    return render_template('sample_be-kan.html')
 
 
 @app.route('/twitter_sample')
@@ -181,78 +248,7 @@ def twitter_sample():
     #     tweet_text.append(tweet["text"])
     #     tweet_time.append(tweet["created_at"])
 
-    recent_tweet_ranking{
-        '1': {
-          'text': 'ついに公開！→【YouTubeテーマソング／ヒカキン＆セイキン 】https://youtu.be/WJzSBLCaKc8  @YouTubeさんから',  # OK
-          'retweet_count':1133,
-          'favolite_count':2019,
-          'date': '2015/08/14'
-        },
-        '2': {
-          'text': 'UUUMから新しいアプリを出しました！マイクラPEやモンストがどこでも話しながらマルチプレイ出来る『おしゃべりマルチ！』是非使ってみて下さい♪
-https://appsto.re/jp/sK0o8.i ',  # OK
-          'retweet_count':943,
-          'favolite_count':1781,
-          'date': '2015/08/20'
-        },
-        '3': {
-          'text': 'YouTubeテーマソング100万再生突破！
-          ご視聴ありがとうございます（ ;  ; ）
-          http://youtu.be/WJzSBLCaKc8 ',  # OK
-          'retweet_count':544,
-          'favolite_count':1842,
-          'date': '2015/08/15'
-        },
-        '4': {
-          'text': '駄目な人のアラームはこちら
-          寝る(-_-)zzz',  # OK
-          'retweet_count':526,
-          'favolite_count':2062,
-          'date': '2015/08/13'
-        },
-        '5': {
-          'text': '夜通しの撮影終了。
-          髪の毛がスプレーでパリパリ。',  # OK
-          'retweet_count':443,
-          'favolite_count':1994,
-          'date': '2015/08/16'
-        }
-    }
-    old_tweet_ranking{
-        '1': {
-          'text': '実家でOFFしてるんでテレビ見てるんですが、凄く可愛い子が出てて気になって調べたら桃色クローバーZってグループのセンターの子だとわかった。これは桃色クローバーZ応援するしかねぇな。。。',  # OK
-          'retweet_count':142,
-          'favolite_count':29,
-          'date': '2012/12/28'
-        }
-        '2': {
-          'text': 'HIKAKIN×PDS×長渕蓮！
-          これからディナーo(^_^)o',  # OK
-          'retweet_count':129,
-          'favolite_count':69,
-          'date': '2012/12/27'
-        }
-        '3': {
-          'text': '地元のスーパーで中学生が声かけてくれた＼(^o^)／僕もセイキンもマスクしてたのにわかるって凄いね。改めてYouTubeやネットの力に驚かされました☆',  # OK
-          'retweet_count':75,
-          'favolite_count':42,
-          'date': '2012/12/29'
-        }
-        '4': {
-          'text': '変態マッサージャーマスオがみんなにマッサージを施し中',  # OK
-          'retweet_count':71,
-          'favolite_count':27,
-          'date': '2012/12/30'
-        }
-        '5': {
-          'text': '松井秀喜さんの引退会見改めて見たけど、カッコよすぎ。。。あの柔らかい感じとか言動の一つ一つから偉大さを感じる。レベルが違いすぎる。。。',  # OK
-          'retweet_count':64,
-          'favolite_count':20,
-          'date': '2012/12/28'
-        }
-    }
-
-    return render_template('sample_nakatomotoi.html', recent_tweet_ranking = recent_tweet_ranking, old_tweet_ranking = old_tweet_ranking)
+    return render_template('sample_nakatomotoi.html')
 
 
 if __name__ == "__main__":
